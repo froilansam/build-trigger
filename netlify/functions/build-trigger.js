45163fb4df78968bb239de4c397f9fac076de5de
Froye <froilansam@gmail.com>
2024-03-22 00:55:19 +0900

    fix

--- netlify/functions/build-trigger.js
+++ netlify/functions/build-trigger.js
@@ -1,7 +1,6 @@
 const { Octokit } = require("octokit");
 
 exports.handler = async (event) => {
-  // Only proceed if the incoming request is a POST request
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
@@ -40,9 +39,50 @@ exports.handler = async (event) => {
 
     console.log(JSON.stringify(res, null, 2));
 
+    // Octokit.js
+    // https://github.com/octokit/core.js#readme
+
+    const workflowRuns = await octokit.request(
+      "GET /repos/{owner}/{repo}/actions/runs",
+      {
+        owner: "readyfastcode",
+        repo: "foodready-mobile",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+
+    if (workflowRuns.workflow_runs?.length) {
+      const lastWorkflowRun = workflowRuns.workflow_runs[0];
+      console.log(
+        `Last workflow run: ${lastWorkflowRun.html_url} - ${lastWorkflowRun.status}`
+      );
+
+      return {
+        statusCode: 200,
+        body: `{
+            "blocks": [
+              {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "*FroBot is now building...* \n\nWorkflow Link: ${lastWorkflowRun.html_url}"
+                },
+                "accessory": {
+                  "type": "image",
+                  "image_url": "https://i.ibb.co/MpvbWcb/ezgif-6-8f0882297a.jpg",
+                  "alt_text": "FroBot building staging and dev-client apps."
+                }
+              }
+            ]
+          }`,
+      };
+    }
+
     return {
       statusCode: 200,
-      body: "GitHub Action triggered successfully!",
+      body: "*FroBot is now building...*\n\nWorkflow Link: https://github.com/readyfastcode/foodready-mobile/actions/workflows/manual-staging-build.yml",
     };
   } catch (error) {
     // Log any errors and return a 500 Internal Server Error status
